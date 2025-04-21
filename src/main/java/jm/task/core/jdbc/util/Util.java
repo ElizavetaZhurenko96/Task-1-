79c50ac9f2d5e01428328e8b5493e1475e44926e
Администратор <Lizock22@mail.ru>
2025-04-21 13:43:22 +0300

    task 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+
 import jm.task.core.jdbc.dao.DbException;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,6 +18,7 @@ public class Util {
     private static final String URL_KEY = "db.url";
     private static final String USER_KEY = "db.user";
     private static final String PASSWORD_KEY = "db.password";
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         try {
@@ -25,5 +32,37 @@ public class Util {
             log.error("Не удалось установить соединение с СУБД");
             throw new DbException(ex.getMessage());
         }
+    }
+
+    public static SessionFactory getSessionFactory() {
+
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+                configuration.setProperty("hibernate.connection.url", PropertiesUtil.get(URL_KEY));
+                configuration.setProperty("hibernate.connection.username", PropertiesUtil.get(USER_KEY));
+                configuration.setProperty("hibernate.connection.password", PropertiesUtil.get(PASSWORD_KEY));
+
+                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+                configuration.setProperty("hibernate.hbm2ddl.auto", "create");
+                configuration.setProperty("hibernate.show_sql", "true");
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                log.info("sessionFactory для Hibernate успешно добавлена");
+
+            } catch (Exception ex) {
+                log.error("sessionFactory для Hibernate не добавлена");
+                throw new DbException(ex.getMessage());
+            }
+            return sessionFactory;
+        }
+        return sessionFactory;
     }
 }
